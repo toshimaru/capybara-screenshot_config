9e94f816beb9b1bb0564f50d9b34ddbb5240adf6
toshi <me@toshimaru.net>
2015-11-26 15:08:31 +0900

    [WIP]Add configure & configuration

--- lib/capybara/screenshot_options.rb
+++ lib/capybara/screenshot_options.rb
@@ -1,7 +1,27 @@
+require 'capybara'
 require "capybara/screenshot_options/version"
+require "capybara/screenshot_options/configuration"
 
 module Capybara
   module ScreenshotOptions
-    # Your code goes here...
+    class << self
+      def configure
+        yield(configuration)
+      end
+
+      def configuration
+        @configuration ||= Configuration.new
+      end
+    end
+  end
+
+  # monkey patching Capybara::Session#save_screenshot
+  class Session
+    alias_method :old_save_screenshot, :save_screenshot
+
+    def save_screenshot(path, options={})
+      # TODO: replace screenshot string
+      old_save_screenshot("screenshot/#{path}", options)
+    end
   end
 end
---
+++ lib/capybara/screenshot_options/configuration.rb
@@ -0,0 +1,7 @@
+module Capybara
+  module ScreenshotOptions
+    class Configuration
+      attr_accessor :foo, :bar
+    end
+  end
+end
