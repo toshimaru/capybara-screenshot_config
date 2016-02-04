51102f310788ab3902854c8fdcab2ad90ecdf21d
toshi <me@toshimaru.net>
2016-02-05 00:43:50 +0900

    Set save_dir

--- lib/capybara/screenshot_options.rb
+++ lib/capybara/screenshot_options.rb
@@ -20,8 +20,7 @@ module Capybara
     alias_method :old_save_screenshot, :save_screenshot
 
     def save_screenshot(path, options={})
-      # TODO: replace screenshot string
-      old_save_screenshot("screenshot/#{path}", options)
+      old_save_screenshot("#{ScreenshotOptions.configuration.save_dir}/#{path}", options)
     end
   end
 end
--- lib/capybara/screenshot_options/configuration.rb
+++ lib/capybara/screenshot_options/configuration.rb
@@ -1,7 +1,12 @@
 module Capybara
   module ScreenshotOptions
     class Configuration
-      attr_accessor :save_dir, :bar
+      attr_accessor :save_dir, :full
+
+      def initialize
+        @save_dir ||= "screenshot";
+        @full     ||= false
+      end
     end
   end
 end
